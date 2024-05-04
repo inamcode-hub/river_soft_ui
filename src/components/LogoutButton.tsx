3462fee5018d49657785308afcd26d857354ced4
inam <inam@dryermaster.com>
2024-05-04 10:44:39 -0400

    fix: Update LogoutButton to use correct returnTo URL

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { useAuth0 } from '@auth0/auth0-react';
-
 import { LogoutOptions } from '@auth0/auth0-react';
 
-const LogoutButton: React.FC  = () => {
+const LogoutButton: React.FC = () => {
     const { logout } = useAuth0();
 
+    
+    const returnToUrl = import.meta.env.VITE_APP_ENV ? window.location.origin : import.meta.env.VITE_VERCEL_URL + '/';
+
     return (
-        <Button variant="contained" color="primary" onClick={() => logout({ returnTo: window.location.origin } as LogoutOptions)}>
+        <Button variant="contained" color="primary" onClick={() => logout({ returnTo: returnToUrl } as LogoutOptions)}>
             Log Out
         </Button>
     );
